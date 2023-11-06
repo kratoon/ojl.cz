1c91fce69e6eb6547909ce265e59a74b5126a977
kratoon <ondrej@kratochvil.dev>
2023-11-06 23:11:16 +0100

    Improve

--- src/MenuItem.js
+++ src/MenuItem.js
@@ -4,9 +4,6 @@ function MenuItem({ title, desc, allergens, quantity, price }) {
             <div className="slds-grid">
                 <div className="slds-col">
                     <div className="name">{title}</div>
-                    <div className="desc">{desc}</div>
-                    {/*{quantity && allergens ? '    ' : ''}*/}
-                    <div className="allergens">{allergens}</div>
                 </div>
                 <div className="slds-col slds-size--3-of-12 slds-col_bump-left">
                     <div className="quantity">{quantity}</div>
@@ -14,6 +11,11 @@ function MenuItem({ title, desc, allergens, quantity, price }) {
                 <div className="slds-col slds-size--2-of-12">
                     <div className="price">{price}</div>
                 </div>
+            </div>
+            <div>
+                <div className="desc">{desc}</div>
+                {/*{quantity && allergens ? '    ' : ''}*/}
+                <div className="allergens">{allergens}</div>
             </div>
         </div>
     );
